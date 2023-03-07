08c59fe6985d67253d4063352a06b8622d7d41fa
injili <nyarekigospel@gmail.com>
2023-03-07 09:19:28 -0800

    the last digit isi it

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print('Last digit of ', number, 'is', end=' ')
-number = abs(number) % 10
-
+print('Last digit of', number, 'is', end=' ')
+num = abs(number) % 10
+if number > 0:
+    num = 0 + num
+else:
+    num = 0 - num
+print(num, end=' ')
+if num > 5:
+    print('and is greater than 5')
+elif num == 0:
+    print('and is 0')
+else:
+    print('and is less than 6 and not 0')
