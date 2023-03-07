b462f9c3c0286c65f1e3bc693bdd1c58d8fdbf28
injili <nyarekigospel@gmail.com>
2023-03-07 14:11:35 -0800

    capitalise on the letters

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
-    i = len(str)
-    for n in range(i):
-        k = ord(str[n])
-        if k > 96 and k < 124:
+    newstr = ""
+    for n in str:
+        if ord(n) > 96 and ord(n) < 124:
+            k = ord(n)
             k -= 32
-            str[n] = chr(k)
-
-
-print(str)
+            character = chr(k)
+            newstr += character
+        else:
+            newstr += n
+    print(newstr)
