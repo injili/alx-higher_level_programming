fbee0abdcfe6eeea49a103e8ee3414bc0c6340d9
injili <nyarekigospel@gmail.com>
2023-03-07 13:17:46 -0800

    is it upper or lower?

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for n in range (0, 100):
+for n in range (0, 10):
     if ((abs(n) % 100) // 10) == (abs(n) % 10):
         continue
     print("{x:02}".format(x=n), end=", " if n < 99 else "\n")
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def islower(c):
+    for n in range(97, 123):
+        if n == ord(c):
+            return True
