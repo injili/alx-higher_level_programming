1820753e403fef93988c54b74abb856ec3606d1d
injili <nyarekigospel@gmail.com>
2023-03-07 16:17:52 -0800

    remove letter at a position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    newstr = ""
+    loop = 0
+
+    if n >= 0:
+        n = n
+    else:
+        i = len(str)
+        n = i + n
+
+
+    for letter in str:
+        if loop == n:
+            loop += 1
+            continue
+        else:
+            newstr += letter
+        loop += 1
+    return newstr
