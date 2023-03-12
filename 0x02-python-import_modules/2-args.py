d788a0fd53cd835cb7992c5794252b6a45a89f01
injili <nyarekigospel@gmail.com>
2023-03-12 11:31:52 -0700

    give arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,10 @@ if __name__ == "__main__":
     num = len(sys.argv) - 1
     i = 1
 
-    print("{} argument".format(num), end="")
     if num > 0:
-        print(":")
+        print("{} arguments:".format(num))
     else:
-        print(".")
+        print("{} argument.".format(num))
 
     while i <= num:
         print("{}: {}".format(i, sys.argv[i]))
