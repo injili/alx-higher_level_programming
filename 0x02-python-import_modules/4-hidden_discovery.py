a5ba069cab01317ad2075727159baeb73523cc38
injili <nyarekigospel@gmail.com>
2023-03-12 13:46:57 -0700

    hidden gems

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     """print out names from within a compiled module"""
     import hidden_4
 
-    names = sorted(dir(hidden_4))
+    names = dir(hidden_4)
     for name in names:
         for name in names:
             if name[:2] != "__":
