48d125a71f4d2c78097dae9e56cf0488b70aa2a2
injili <nyarekigospel@gmail.com>
2023-03-13 01:00:18 -0700

    list print

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-
     i = 0
     num = len(my_list)
     while i < num:
-        print("{}".format(int(my_list[i])))
+        print(my_list[i])
         i += 1
