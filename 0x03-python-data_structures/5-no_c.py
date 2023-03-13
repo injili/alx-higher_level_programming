a7fd785bc4425e2c9f04b02ed63c2df92cea3c30
injili <nyarekigospel@gmail.com>
2023-03-13 05:52:35 -0700

    delete string literal

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = my_string
     for i in range(0, len(my_string)):
         if my_string[i] == "c" or my_string[i] == "C":
-            new = my_string[:i] + my_string[i + 1:]
-    return new
+            new_string = new_string[:i] + new_string[i + 1:]
+    return new_string
