8640fba5eb661ea62e4ca192ae7bf41fbcfca971
injili <nyarekigospel@gmail.com>
2023-03-14 00:25:02 -0700

    matrix escape

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if len(matrix) > 1:
         for i in range(0, len(matrix)):
             for j in range(0, len(matrix[i])):
-                print("{:d}".format(int(matrix[i][j])), end=" " if j < len(matrix[i]) - 1 else "\n")
+                print("{:d}".format(int(matrix[i][j])), end=" "
+                      if j < len(matrix[i]) - 1 else "\n")
     else:
         print("")
