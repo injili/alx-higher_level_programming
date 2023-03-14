ef434c8037944ec0c5e911cbb25c35d3016bd239
injili <nyarekigospel@gmail.com>
2023-03-14 02:06:29 -0700

    greatness tupu

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    greatest = 0
     if len(my_list) == 0:
         return None
     else:
         for i in range(0, len(my_list)):
             for j in range(0, len(my_list)):
-                if my_list[i] < my_list[j]:
+                if my_list[i] < my_list[j] and my_list[j] > greatest:
                     greatest = my_list[j]
-                    break
     return greatest
