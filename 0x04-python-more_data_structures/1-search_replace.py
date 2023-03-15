c729c1d85fcd6ce9fdf36dd936bc00b232035d28
injili <nyarekigospel@gmail.com>
2023-03-15 11:14:34 -0700

    add to

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,8 +3,8 @@ def search_replace(my_list, search, replace):
     if not my_list:
         return None
     else:
-        new_list = my_list
+        new_list = my_list.copy()
         for i in range(0, len(my_list)):
             if my_list[i] == search:
                 new_list[i] = replace
-        return new_list
+    return new_list
