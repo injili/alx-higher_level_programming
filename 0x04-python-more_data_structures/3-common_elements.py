da8d26f577a862215822c44edef1f5fd4c65da21
injili <nyarekigospel@gmail.com>
2023-03-15 12:44:11 -0700

    do it

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    if not set_1 or not set_2:
+        return None
+    else:
+        new_list = []
+        for i in set_1:
+            for j in set_2:
+                if i == j and :
+                    new_list.append(j)
+        return new_list
