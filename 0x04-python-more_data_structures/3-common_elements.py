aeb6b6f856782671a43fb6d8bb4eab2eb4ea9f34
injili <nyarekigospel@gmail.com>
2023-03-15 12:51:11 -0700

    do it

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,9 +3,11 @@ def common_elements(set_1, set_2):
     if not set_1 or not set_2:
         return None
     else:
-        new_list = []
+        new_list = [elem for elem in set_1 if elem in set_2]
+        '''
         for i in set_1:
             for j in set_2:
                 if i == j and :
                     new_list.append(j)
+         '''
         return new_list
