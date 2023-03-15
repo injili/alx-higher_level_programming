279d5ae29de0f8a0068f22b68992a35a28183842
injili <nyarekigospel@gmail.com>
2023-03-15 14:42:42 -0700

    not in the other

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,12 @@
+#usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    if not set_1:
+        return set_2
+    elif not set_2:
+        return set_1
+    else:
+        new1 = set_1.difference(set_2)
+        new2 = set_2.difference(set_1)
+        new3 = new1 | new2
+
+        return new3
