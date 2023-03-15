945a302cf6b744aa53c3db862eff3480bd0dd0b3
injili <nyarekigospel@gmail.com>
2023-03-15 12:01:34 -0700

    add unique elements of a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    if not my_list:
+        return 0
+    else:
+        sum_uniq = 0
+        for i in my_list:
+            count = 0
+            for j in range(0, len(my_list)):
+                if i == my_list[j]:
+                    count += 1
+                if count > 1:
+                    del my_list[j]
+                    break
+        for a in my_list:
+            sum_uniq += a
+        return sum_uniq
