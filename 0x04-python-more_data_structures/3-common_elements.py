d9c7cb1186afb882c970d06d77b54f206e845f11
injili <nyarekigospel@gmail.com>
2023-03-15 13:06:00 -0700

    do it

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if not set_1 or not set_2:
-        return None
+    if not set_1:
+        return set_1
+    elif not set_2:
+        return set_2
     else:
-        one = set(set_1)
-        two = set(set_2)
-
-        intersection = one.intersection(two)
-        return list(intersection)
+        intersection = set_1.intersection(set_2)
+        return intersection
