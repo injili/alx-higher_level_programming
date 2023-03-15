1504474c5e308cd47cacb0eb92cd5ef87ad410a6
injili <nyarekigospel@gmail.com>
2023-03-15 11:04:53 -0700

    add to

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if not my_list:
+        return None
+    else:
+        new_list = my_list
+        for i in range(0, len(my_list)):
+            if my_list[i] == search:
+                new_list[i] = replace
+        return new_list
