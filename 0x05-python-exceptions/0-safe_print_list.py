55bac8ada9f5f4710a6dd92b562c8dfa29789cb9
injili <nyarekigospel@gmail.com>
2023-03-29 03:30:09 -0700

    print out the list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,8 +11,9 @@ def safe_print_list(my_list=[], x=0):
 
     while y < length:
         if i > x:
+            #print("\n")
             break
-        print("{}".format(my_list[y]), end="" if y + 1 < length or i < x else "\n")
+        print("{}".format(my_list[y]), end="" if i < x else "\n")
         i += 1
         y += 1
         count += 1
