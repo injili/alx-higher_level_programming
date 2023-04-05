74969cd25a48ea4747ef2338ab6873c0beb549ab
injili <nyarekigospel@gmail.com>
2023-04-05 02:53:57 -0700

    private attribute size defining a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class Square tht defines a square by:
+    private instances attribute: size
+    instantiation with size
+    """
+
+
+class Square:
+    """Initialization of the class."""
+    def __init__(self, size):
+        """the instantation."""
+        self.__size = size
