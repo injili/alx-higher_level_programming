411d1d319b51f8f1ceb132c2edcf5c9836304554
injili <nyarekigospel@gmail.com>
2023-06-04 08:20:30 -0700

    new

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
+"""
+An integer addition function
+The function takes in one or two integer or float values
+Returns value or error depending on the input
+"""
 
 def add_integer(a, b=98):
-    """
-    Return the integer addition a and b
+    """Return the integer addition a and b
     Float values are accepted and cast into integers
-    Raises:
-        TypeError: a must be an integer or b must be an integer
     """
-    if type(a) != float and type(a) != int: 
+    if type(a) != float and type(a) != int:
         raise TypeError("a must be an integer")
     if type(b) != float and type(b) != int:
         raise TypeError("b must be an integer")
-    
+
     return (int(a) + int(b))
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
-
