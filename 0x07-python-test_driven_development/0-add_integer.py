5abf7860027cdaece230005b85efd01a4f659d7c
injili <nyarekigospel@gmail.com>
2023-06-04 04:45:25 -0700

    adds two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,2 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """
+    Return the integer addition a and b
+    Float values are accepted and cast into integers
+    Raises:
+        TypeError: a must be an integer or b must be an integer
+    """
+    if type(a) != float and type(a) != int: 
+        raise TypeError("a must be an integer")
+    if type(b) != float and type(b) != int:
+        raise TypeError("b must be an integer")
+    
+    return (int(a) + int(b))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
