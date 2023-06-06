18493c4bce72c3481eaa85297c2ca3e14c9e6e7f
injili <nyarekigospel@gmail.com>
2023-06-06 14:23:48 -0700

    add integer and its tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """
-An integer addition function
-The function takes in one or two integer or float values
-Returns value or error depending on the input
+Module 0-add_integer
+Function add_integer takes int or float values
+Returns an int sum
 """
 
+
 def add_integer(a, b=98):
-    """Return the integer addition a and b
-    Float values are accepted and cast into integers
+    """
+    Returns a + b
     """
     if type(a) != float and type(a) != int:
         raise TypeError("a must be an integer")
-    if type(b) != float and type(b) != int:
+    elif type(b) != float and type(b) != int:
         raise TypeError("b must be an integer")
-
-    return (int(a) + int(b))
+    else:
+        return int(a) + int(b)
