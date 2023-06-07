c5d3a94e96d1c9c926ad32a503febb6c7d86d44f
injili <nyarekigospel@gmail.com>
2023-06-07 11:25:23 -0700

    indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 5-text_indentation
+A function that prints a text with 2 new lines
+after each of the following characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    
