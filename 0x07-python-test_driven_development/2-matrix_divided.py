fd15aba0b29ce402cc174aa954e742ea6a8220e0
injili <nyarekigospel@gmail.com>
2023-06-07 01:30:55 -0700

    matrix division function

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,19 +10,24 @@ def matrix_divided(matrix, div):
     """
     returns the matrix / div
     """
+    new_matrix = []
+
     msg = "matrix must be a matrix (list of lists) of integers/floats"
     msg1 = "Each row of the matrix must have the same size"
+
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     elif div == 0:
         raise ZeroDivisionError("division by zero")
+    elif not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError(msg1)
     else:
         for i in range(len(matrix)):
+            new_row = []
             for j in range(len(matrix[i])):
                 if type(matrix[i][j]) != int and type(matrix[i][j]) != float:
                     raise TypeError(msg)
-                elif len(matrix) != len(matrix[i]):
-                    raise TypeError(msg1)
                 else:
-                    matrix[i][j] = round((matrix[i][j] / div), 2)
-    return (matrix)
+                    new_row.append(round((matrix[i][j] / div), 2))
+            new_matrix.append(new_row)
+    return (new_matrix)
