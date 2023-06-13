ef02d8f375880de91fb267f5da56f4b7ea6e6c88
injili <nyarekigospel@gmail.com>
2023-06-13 10:28:48 -0700

    convert to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+"""
+module 3-to_json
+a function that returns the JSON rep of an objective string
+"""
+
+
+def to_json_string(my_obj):
+    """
+    takes in an object returning it's JSON representation
+    """
+    return(json.dumps(my_obj))
