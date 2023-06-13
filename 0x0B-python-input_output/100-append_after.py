7586ce097ad3a0d3eaa17b9d378c4d11cf942072
injili <nyarekigospel@gmail.com>
2023-06-13 14:54:57 -0700

    add new line

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,13 +14,11 @@ def append_after(filename="", search_string="", new_string=""):
         search_string: string to be found
         new_string: string to be appended
     """
-    with open(filename, mode="r", encoding="utf-8") as f:
-        lines = f.readlines()
-    with open(filename, mode="w", encoding="utf-8") as f:
+    with open(filename, mode="r+", encoding="utf-8") as f:
         new_text = ""
-        for line in lines:
-            f.write(line)
+        for line in f:
+            new_text += line
             if search_string in line:
-                line.write("\n" + new_string + "\n")
+                new_text += new_string
         f.seek(0)
         f.write(new_text)
