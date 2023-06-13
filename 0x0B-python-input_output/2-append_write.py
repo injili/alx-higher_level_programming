513bbb8ea5c4ec531b5f874e6e3549c140b20131
injili <nyarekigospel@gmail.com>
2023-06-13 08:29:54 -0700

    append new data

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module 2-append_write
+a function that appends a string to the end of the text file
+returns the number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    opens the file in append mode
+    """
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return (f.append(text))
