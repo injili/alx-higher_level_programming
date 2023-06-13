c3d4ff143d5f2955a5c4b11317837bf44cbec3cc
injili <nyarekigospel@gmail.com>
2023-06-13 14:45:58 -0700

    add new line

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+module 100-append_after
+A function that inserts a line of text to a file
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    inserts line after a specific string file
+
+    Args:
+        filename: name of the file to mod
+        search_string: string to be found
+        new_string: string to be appended
+    """
+    with open(filename, mode="r", encoding="utf-8") as f:
+        lines = f.readlines()
+    with open(filename, mode="w", encoding="utf-8") as f:
+        for line in lines:
+            f.write(line)
+            if line == search_string:
+                line.write("\n" + new_string + "\n")
