e258201adf83aca362d19a9d58764c6e9a6ae16d
injili <nyarekigospel@gmail.com>
2023-06-13 14:19:23 -0700

    add items using other functions

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module 8-class_to_json
+a function that returns the dictionary description
+with simple data structure for JSON serialization
+of an object
+"""
+
+
+def class_to_json(obj):
+
