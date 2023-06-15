c90203b2327c9d3904aa236f3b746e07f0bc5fdb
injili <nyarekigospel@gmail.com>
2023-06-15 04:42:41 -0700

    out now

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,9 +16,9 @@ class Rectangle:
 
     """property setter"""
     def width(self, value):
-        if type(self.__width) != int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
-        elif self.__width < 0:
+        elif value < 0:
             raise ValueError("width must be >=0")
         else:
             self.__width = value
@@ -29,9 +29,9 @@ class Rectangle:
 
     """property setter"""
     def height(self, value):
-        if type(self.__height) != int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
-        elif self.__height < 0:
+        elif value < 0:
             raise ValueError("height must be >=0")
         else:
             self.__height = value
