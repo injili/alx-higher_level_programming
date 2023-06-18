b94a6ee5238705e14ac341411dd676e5e698045b
injili <nyarekigospel@gmail.com>
2023-06-18 05:10:27 -0700

    rectangle instance

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+module rectangle
+inherits from the class Base
+"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    an instanciation of the class rectangle
+
+    Args:
+        width(int): the width of rectangle
+        height(int): the height of rectangle
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """intialize"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
