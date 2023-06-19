578202521c9ccafa8f653e0ff4aeff924aa6d775
injili <nyarekigospel@gmail.com>
2023-06-19 09:31:24 -0700

    return the json representation of the dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,8 @@ the base of all other classes on this project
 """
 
 
+import json
+
 class Base:
     """
     This is the base class to evey other class
@@ -19,3 +21,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))
