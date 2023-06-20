17c8d8f64ab684ec1717d84fb346c614a2b1d7b0
injili <nyarekigospel@gmail.com>
2023-06-19 22:15:39 -0700

    not now yet

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,11 +6,13 @@ unittest for Rectangle class
 """
 
 
+import os
 import unittest
 import pep8
-import models.rectangle
+from models import rectangle
 from io import StringIO
 from contextlib import redirect_stdout
+rectangle = rectangle.Rectangle
 
 class TestPep8(unittest.TestCase):
     """Pep8 models/rectangle.py and tests/test_modules/test_rectangle.py"""
@@ -18,3 +20,29 @@ class TestPep8(unittest.TestCase):
         """pep8"""
         style = pep8.StyleGuide(quiet=False)
         errors = 0
+        files = ["models/rectangle.py", "tests/test_models/test_rectangle.py"]
+        errors += style.check_files(files).total_errors
+        self.assertEqual(errors, 0, 'Fix Pep8')
+
+class TestBase(unittest.TestCase):
+    """tests for the module models/rectangle"""
+
+    def test_all_attributes_given(self):
+        """Test all the attributes match"""
+        r1 = Rectangle(15, 25, 1, 2, 99)
+        self.assertTrue(r1.width == 15)
+        self.assertTrue(r1.height == 25)
+        self.assertTrue(r1.x == 1)
+        self.assertTrue(r1.y == 2)
+        self.assertTrue(r1.id == 99)
+
+    def test_default_attributes(self):
+        """Test default attributes"""
+        r2 = Rectangle(10, 20)
+        self.assertTrue(r2.width == 10)
+        self.assertTrue(r2.height == 20)
+        self.assertTrue(r2.x == 0)
+        self.assertTrue(r2.y == 0)
+        self.assertTrue(r2.id is not None)
+
+    def 
