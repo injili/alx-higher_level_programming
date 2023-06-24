d7ea8ebb4ca2653e4f072ee93dee3a3021118225
injili <nyarekigospel@gmail.com>
2023-06-23 22:10:08 -0700

    max integer

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module 6-max_integer
+tests
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    run with python3 -m unittest -v tests/6-max_integer_test.py
+    """
+    def test
