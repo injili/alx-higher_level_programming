760ea5baa67f85c29d56eb19a2f595b51de866b8
injili <nyarekigospel@gmail.com>
2023-06-24 07:09:19 -0700

    matrix multiplication

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+module 100-matrix_mul
+A method that does matrix multiplication
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """
+    Returns the resulting matrix
+    """
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+
+    for row in m_a:
+        for element in row:
+            if not isinstance(element, int) and not isinstance(element, float):
+                raise TypeError("m_a should contain only integers or floats")
+
+    for row in m_b:
+        for element in row:
+            if not isinstance(element, int) and not isinstance(element, float):
+                raise TypeError("m_b should contain only integers or floats")
+
+    count = len(m_a[0])
+    for row in m_a:
+        if len(row) != count:
+            raise TypeError("each row of m_a must be of the same size")
+
+    count = len(m_b[0])
+    for row in m_b:
+        if len(row) != count:
+            raise TypeError("each row of m_b must be of the same size")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    result = [[0 for _ in range(len(m_b[0]))] for _ in range(len(m_a))]
+
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_a[0])):
+                result[i][j] += m_a[i][k] * m_b[k][j]
+
+    return result
