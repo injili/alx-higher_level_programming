94701fa8fb2eb65286b3713fce138030bfaf0ef8
injili <nyarekigospel@gmail.com>
2023-06-23 21:34:03 -0700

    spaces and tabs

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,4 +7,14 @@ after each of the following characters: ., ? and :
 
 
 def text_indentation(text):
-    
+    """
+    Prints text with two new lines after '.', '?' and ':'
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for char in ".?:":
+        text = text.replace(char, char + "\n\n")
+    l = [lines.strip(' ') for lines in text.split('\n')]
+    rev = "\n".join(l)
+    print(rev, end="")
