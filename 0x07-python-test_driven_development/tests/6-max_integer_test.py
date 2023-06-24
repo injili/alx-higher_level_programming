6061f6f3b63a10f7473b4a89ba810b64665e4999
injili <nyarekigospel@gmail.com>
2023-06-23 22:20:11 -0700

    max integer

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,4 +10,10 @@ class TestMaxInteger(unittest.TestCase):
     """
     run with python3 -m unittest -v tests/6-max_integer_test.py
     """
-    def test
+    def test_the_module_docstring(self):
+        moduleDoc = __import__('6-max_integer').__doc__
+        self.assertTrue(len(moduleDoc) > 1)
+
+    def test_the_function_docstring(self):
+        functionDoc = __import__('6-max_integer').max_integer.__doc__
+        self.assertTrue(len(functionDoc) > 1)
