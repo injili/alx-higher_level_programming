f2e442d2aa478dbb2b1fda70d94c805026d5bf57
injili <nyarekigospel@gmail.com>
2023-06-23 22:47:47 -0700

    max integer

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -17,3 +17,23 @@ class TestMaxInteger(unittest.TestCase):
     def test_the_function_docstring(self):
         functionDoc = __import__('6-max_integer').max_integer.__doc__
         self.assertTrue(len(functionDoc) > 1)
+
+    def test_signed_ints_and_floats(self):
+        self.assertEqual(max_integer([0]), 0)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 2, 3, -4]), 3)
+        self.assertEqual(max_integer([-1.5, -2, -3]), -1.5)
+        self.assertEqual(max_integer([10, -10, 10]), 10)
+        self.assertEqual(max_integer([{1, 9}, {3}, {4}]), {1, 9})
+
+    def test_strings(self):
+        self.assertEqual(max_integer("1279"), '9')
+        self.assertEqual(max_integer("abyfz"), 'z')
+        self.assertEqual(max_integer(['a', 'b', 'c', 'x']), 'x')
+        self.assertEqual(max_integer(["wbn", 'z']), 'z')
+
+    def test_lists(self):
+        self.assertEqual(max_integer([[1, 3], [1, 1]]), [1, 3])
+
+    def test_error_handling(self):
+        with self.assertRaises
