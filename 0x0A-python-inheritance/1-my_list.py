3d9c3b44884ada75e205af941beaeb46ddc9870f
injili <nyarekigospel@gmail.com>
2023-07-03 13:14:56 -0700

    we try agin in the morning

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module 1-my_list
+"""
+
+
+class MyList(list):
+    """
+    a class that inherits from list
+    """
+    def print_sorted(self):
+        print(list)
+        return (list)
