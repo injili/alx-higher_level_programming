6f6d79e1e5ae13558f9cd4973b5a79b6d2ed9ebb
injili <nyarekigospel@gmail.com>
2023-07-04 13:42:40 -0700

    sorted list init

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module 1-my_list
+"""
+
+
+class MyList(list):
+    """
+    a class that inherits from the list
+    """
+    def print_sorted(self):
+        print(sorted(self))
