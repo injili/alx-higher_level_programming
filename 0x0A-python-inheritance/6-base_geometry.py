3906c0326197f568db581854ec8fce6a40a08941
injili <nyarekigospel@gmail.com>
2023-07-08 23:58:30 -0700

    area is not implemented is it?

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module 5-base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    based on the 5-base_geometry class
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
