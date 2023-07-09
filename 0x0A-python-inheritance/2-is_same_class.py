3d36e103f0abcfd8538b2c91be560f6c4ca490e9
injili <nyarekigospel@gmail.com>
2023-07-08 23:03:26 -0700

    is it an instance or not?

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+module 2-is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    a function that returns True if the object is
+    exactly an instance of the specifird class;
+    otherwise, False
+
+    Args
+    obj: the object passed to the class
+    a_class: the class in comparison
+    """
+
+    return (isinstance(obj, a_class))
