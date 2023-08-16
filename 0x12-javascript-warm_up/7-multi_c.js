17f51f35c13a67454afc6588a5defc5abee8c958
injili <nyarekigospel@gmail.com>
2023-08-16 12:19:23 -0700

    C as many times as I can

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/*
+ * A script that prints "C is fun" as many times as possible.
+ */
+
+const times = parseInt(process.argv[2]);
+
+if (isNaN(times)) {
+  console.log('Missing number of occurrences');
+} else if (times > 0) {
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
+  }
+} else {
+}
