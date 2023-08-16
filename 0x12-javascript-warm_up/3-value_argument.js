d379e50e49c44810c7aa27158b153c7ef4ccfe04
injili <nyarekigospel@gmail.com>
2023-08-16 11:26:15 -0700

    value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,10 +4,8 @@
  * Value of my argument
  */
 
-const { argv } = require('node:process');
-
-if (!process.argv[2]){
-	console.log('No Argument');
-}else{
-	console.log(process.argv[2]);
+if (!process.argv[2]) {
+  console.log('No Argument');
+} else {
+  console.log(process.argv[2]);
 }
