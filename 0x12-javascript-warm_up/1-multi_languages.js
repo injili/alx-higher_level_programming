b59e8afe2feb74116cdf12abc0f8f93971d1156e
injili <nyarekigospel@gmail.com>
2023-08-16 10:08:16 -0700

    all three languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/*
+ * A script that prints output in three lines
+ */
+
+const first = 'C is fun';
+const second = 'Python is cool';
+const third = 'Javascript is amazing';
+
+console.log(first + '\n' + second + '\n' + third);
