859e6fcd5e23a51eba64138582c8391cc4838127
injili <nyarekigospel@gmail.com>
2023-08-18 09:28:42 -0700

    add two numbers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -7,9 +7,8 @@
 const first = parseInt(process.argv[2]);
 const second = parseInt(process.argv[3]);
 
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
 console.log(add(first, second));
-
