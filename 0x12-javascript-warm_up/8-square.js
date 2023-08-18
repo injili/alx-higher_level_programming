3252801868fde82885f063f26837f287049dc2c3
injili <nyarekigospel@gmail.com>
2023-08-18 09:25:32 -0700

    add two numbers

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-/**
+/*
  * A script that prints a square
  */
 
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/*
+ * A script that prints the addition of 2 integers
+ */
+
+const first = parseInt(process.argv[2]);
+const second = parseInt(process.argv[3]);
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(first, second));
+
