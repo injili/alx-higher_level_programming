aa6fd9b722de46c271348ce7134770b1dcbb600b
injili <nyarekigospel@gmail.com>
2023-08-18 19:25:03 -0700

    finding the second largest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,4 +4,14 @@
  * A script that searches the second biggest integer in the list of arguments
  */
 
+const theArray = process.argv.slice(2);
 
+function findSecond (arr) {
+  if (arr.length < 2) {
+    return 0;
+  }
+  arr.sort((x, y) => y - x);
+  return arr[1];
+}
+
+console.log(findSecond(theArray));
