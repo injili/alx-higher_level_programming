cd3d3250f2f37fc310899413a2ba5f0312c2dae5
injili <nyarekigospel@gmail.com>
2023-08-24 14:02:25 -0700

    double and rotate them values

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+/*
+ * Draw the rectangle using x.
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let arr = '';
+      for (let j = 0; j < this.width; j++) {
+        arr += 'X';
+      }
+      console.log(arr);
+    }
+  }
+
+  rotate() {
+    let partial = this.width;
+    this.width = this.height;
+    this.height = partial;
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
