f20caf7e80bda3f5a0a4aa8b1afc453fafa6e5c5
injili <nyarekigospel@gmail.com>
2023-08-26 14:18:44 -0700

    list all from states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
 import MySQLdb
-import sys
+from sys import argv
 
 if len(sys.argv) = 4:
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     try:
-        db = connect(host='localhost', port='3306', database='hbtn_0e_0_usa')
+        db = connect(host='localhost',
+                     port='3306',
+                     user=username,
+                     passwd=password,
+                     db=database)
 
         cursor = db.cursor()
 
