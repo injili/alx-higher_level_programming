24b18fac539b9339992560625e67a39e8805e330
injili <nyarekigospel@gmail.com>
2023-08-26 14:36:03 -0700

    list all from states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,15 +9,17 @@ if __name__ = "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = connect(host="localhost",
-                 port=3306,
-                 user=username,
-                 passwd=password,
-                 db=database)
+    # connsect to the database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
 
+    # create cursor
     cursor = db.cursor()
 
-    query = "SELECT * FROM states"
+    query = "SELECT * FROM states ORDER BY id ASC"
     cursor.execute(query)
 
     result = cursor.fetchall()
