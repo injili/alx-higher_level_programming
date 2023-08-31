6adc3c1349be5b58dd8cbc71ce1779d42021c1d3
injili <nyarekigospel@gmail.com>
2023-08-31 15:13:51 -0700

    cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that takes in a URL and displays all HTTP methods the serer will accept
-curl -sI "$1" | grep 'Allow:' | cut -f2 -d '
+curl -sI "$1" | grep 'Allow:' | cut -f2 -d' '
