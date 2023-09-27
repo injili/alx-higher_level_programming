01634b66639cf1302d4294a6e07c1dd9eb99314b
injili <nyarekigospel@gmail.com>
2023-09-27 09:23:49 -0700

    request the status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-let theUrl = process.argv[2];
+const theUrl = process.argv[2];
 request(theUrl, function (error, response) {
   if (error) {
     console.log(error);
