9e01e341282b1bf365b1758a9b3f7544a50ca51f
injili <nyarekigospel@gmail.com>
2023-09-27 08:36:16 -0700

    the source code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const theUrl = process.argv[2];
+request(theUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log('code: ', response.statusCode);
+});
