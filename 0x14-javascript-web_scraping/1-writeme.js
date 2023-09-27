6d4374a45ee16555af27ae7802f65d86fbcc003c
injili <nyarekigospel@gmail.com>
2023-09-27 06:35:43 -0700

    write onto a new file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const  filename = process.argv[2];
+const filename = process.argv[2];
 const whatToWrite = process.argv[3];
 
 fs.writeFile(filename, whatToWrite, 'utf8', (err) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-})
+  if (err) {
+    console.log(err);
+  }
+});
