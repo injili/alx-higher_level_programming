bc84870f16ba085d042d97470b24c4fa3c95f967
injili <nyarekigospel@gmail.com>
2023-09-28 05:10:13 -0700

    Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -9,13 +9,13 @@ request(url, function (err, response, body) {
     console.log(err);
   } else if (response.statusCode === 200) {
     body = JSON.parse(body);
-    for (let i in body.characters) {
+    for (const i in body.characters) {
       request(body.characters[i], function (err, response, body) {
-	if (err) {
-	  console.log(err);
-	} else if (response.statusCode === 200) {
-	  console.log(JSON.parse(body).name);
-	}
+        if (err) {
+          console.log(err);
+        } else if (response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
       });
     }
   } else {
