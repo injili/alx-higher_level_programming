cb6e249815c32b256268cb471aa7d335ef5bd240
injili <nyarekigospel@gmail.com>
2023-09-28 04:40:25 -0700

    loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,10 +5,8 @@ const filename = process.argv[3];
 const fs = require('fs');
 const request = require('request');
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    fs.writeFile(filename, body, 'utf8');
+request(baseURL, (error, response, body) => {
+  if (error == null) {
+    fs.writeFileSync(bodyResp, body);
   }
 });
